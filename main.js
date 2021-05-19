922f17ecbc08b0f75d92ef7426567e534a3c47bf
ayrtonteshima <ayrtonteshima@gmail.com>
2021-05-18 23:43:18 -0300

    código aula

---
+++ main.js
@@ -0,0 +1,5 @@
+import multiply, { soma, sub } from './mods/math.js';
+
+console.log(multiply(7, 5));
+console.log(sub(5, 2));
+console.log(soma(5, 2));
---
+++ mods/math.js
@@ -0,0 +1,4 @@
+export { default as soma } from '/mods/sum.js';
+export { default as sub } from '/mods/sub.js';
+export { default } from '/mods/multiply.js';
+export { default as divide } from '/mods/divide.js';
